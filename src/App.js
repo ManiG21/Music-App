973b359c611370b755eab31a057aff55d368587f
ManiG21 <manigrant023@gmail.com>
2023-01-04 19:47:27 -0500

    Logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-// import logo from './logo.svg';
+import logo from './brilliant dreams.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <header className="Landing">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <button className='Start'> Join The Experience</button>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <button className='Start'> Join The Experience</button>
     </div>
   );
 }
